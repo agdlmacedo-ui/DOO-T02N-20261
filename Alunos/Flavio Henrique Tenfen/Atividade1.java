b0dfe27476b59b0e71cfeb2edfa440f1c19097b7
Flavio <flaviofht.tenfen@gmail.com>
2026-02-25 23:53:13 -0300

    Atividade aula 01

---
+++ Alunos/Flavio Henrique Tenfen/Atividade1.java
@@ -0,0 +1,11 @@
+public class Atividade1 {
+    
+    public static void main(String[] args) {
+        
+    
+    System.out.println("Hello Word!");
+    System.out.println("Hi Flavio Tenfen!");
+
+}
+
+}
